46b486c553886c17b61f3bfafc2e5491e0cbdaed
albiosz <szulcalbert@gmail.com>
2019-04-17 14:27:55 +0200

    new

--- Topological sorting.py
+++ Topological sorting.py
@@ -1,3 +1,33 @@
-print("Topological sorting")
-for num in range(1,129):
-    print(num)
+class Vertex:
+    def __init__(self,name):
+        self.name = name
+
+class Graph:
+    def __init__(self,size):
+        self.size = size
+        self.matrix = [[0]*size]*size
+        self.vertices = {}
+
+    def display_matrix(self):
+        print("  ", end="")
+        for num in range(self.size):
+            print(str(num)+" ", end="")
+        print()
+        print("  ", end="")
+        for num in range(self.size):
+            print("--", end="")
+        print()
+        for ind,num in enumerate(self.matrix):
+            print(str(ind)+"|",end="")
+            for n in num:
+                print(str(n)+" ",end='')
+            print()
+
+    def add_vertex(self,vertex):
+        pass
+
+
+
+v = Vertex("A")
+g = Graph(10)
+g.display()
