f1a01019c971cd912387c09e1288f345660e1ddd
Albert <szulcalbert@gmail.com>
2019-04-22 10:55:45 +0200

    Ukończone topologiczne sorty (macierz + lista) bez komentów

--- Topological sorting.py
+++ Topological sorting.py
@@ -43,11 +43,14 @@ class GraphMatrix:
     def show_edge(self, y, x):
         return self.matrix[y][x]
 
-    def fill_graph(self,per):
+    def fill_graph(self,per): #funkcja wypełniająca graf w podanym nasyceniu
         num_of_vertices = (self.size*(self.size-1)/2)*(per/100)
-        for num in range(self.size - 1):
-            self.add_edge(num, num+1)
+        s = 0
+        while s < self.size - 1 and num_of_vertices > 0: # na początku zapełniamy graf na żeby był spójny
+            self.add_edge(s, s+1)
+            print((s, s+1))
             num_of_vertices -= 1
+            s += 1
         while num_of_vertices > 0:
             y = random.randint(0, self.size-2)
             x = random.randint(y+1, self.size-1)
@@ -80,6 +83,7 @@ class GraphList:
     def __init__(self):
         self.lst = []
         self.vertices = []
+        self.edges = []
 
     def add_vertex(self,x):
         if x not in self.vertices:
@@ -90,17 +94,44 @@ class GraphList:
             self.lst.append(element)
             element.append(x)
 
+    # def add_edge(self, y, x):
+    #     if y in self.vertices and x in self.vertices:
+    #         for el in self.lst:
+    #             first_el = el.head.next
+    #             if first_el.value == y:
+    #                 el.append(x)
+    #                 break
+
     def add_edge(self, y, x):
-        if y in self.vertices and x in self.vertices:
+        if (y, x) not in self.edges:
+            self.edges.append((y,x))
             for el in self.lst:
                 first_el = el.head.next
                 if first_el.value == y:
                     el.append(x)
-                    break
+                    return True
+        else:
+            return False
 
     def display(self):
+        disp = [] # lista z kolejnymi elementami listy grafu
         for el in self.lst:
-            print(el.display())
+            disp.append(el.display())
+        return disp
+
+    def fill_graph(self, per): # funkcja wypełniająca graf w podanym nasyceniu
+        size = len(self.vertices)
+        num_of_vertices = (size*(size-1)/2)*(per/100)
+        s = 0
+        while s < size - 1 and num_of_vertices > 0: # na początku zapełniamy graf żeby był spójny
+            self.add_edge(s, s+1)
+            num_of_vertices -= 1
+            s += 1
+        while num_of_vertices > 0:
+            y = random.randint(0, size-2)
+            x = random.randint(y+1, size-1)
+            if self.add_edge(y, x):
+                num_of_vertices -= 1
 
     def topological_sort(self):
         visited = {}
@@ -120,13 +151,13 @@ class GraphList:
         while True:
             #print(visited, tp_sorted, v.value)
             if v.next is not None:
+                v = v.next
                 #print(visited[v.next.value])
-                if visited[v.next.value] == 0:
-                    v = v.next
-                    v = self.lst[v.value].head.next  #
-                    self.ts(v, visited, tp_sorted)
-                else:
-                    v = v.next
+                if visited[v.value] == 0:
+                    for e in self.lst:
+                        if v.value == e.head.next.value:
+                            self.ts(e.head.next, visited, tp_sorted)
+                            break
             else:
                 break
 
@@ -135,30 +166,36 @@ class GraphList:
 
 
 gl = GraphList()
-gl.add_vertex(0)
-gl.add_vertex(1)
-gl.add_vertex(2)
-gl.add_vertex(3)
-gl.add_vertex(4)
-gl.add_vertex(5)
-gl.add_vertex(6)
-gl.add_vertex(7)
-gl.add_vertex(8)
-gl.add_vertex(9)
-gl.add_vertex(10)
-gl.add_vertex(11)
-gl.add_edge(5, 11)
-gl.add_edge(11, 2)
-gl.add_edge(11, 9)
-gl.add_edge(11, 10)
-gl.add_edge(7, 11)
-gl.add_edge(7, 8)
-gl.add_edge(8, 9)
-gl.add_edge(3, 8)
-gl.add_edge(3, 10)
-gl.display()
-print(gl.vertices)
-print(gl.topological_sort())
+for num in range(10):
+    gl.add_vertex(num)
+gl.fill_graph(60)
+print(gl.display())
+
+
+# gl = GraphList()
+# gl.add_vertex(0)
+# gl.add_vertex(1)
+# gl.add_vertex(2)
+# gl.add_vertex(3)
+# gl.add_vertex(4)
+# gl.add_vertex(5)
+# gl.add_vertex(6)
+# gl.add_vertex(7)
+# print(gl.add_edge(7, 5))
+# gl.add_edge(7, 5)
+# gl.add_edge(7, 6)
+# gl.add_edge(6, 3)
+# gl.add_edge(6, 4)
+# gl.add_edge(5, 4)
+# gl.add_edge(5, 2)
+# gl.add_edge(2, 1)
+# gl.add_edge(3, 1)
+# gl.add_edge(1, 0)
+# print(gl.display())
+# print(gl.vertices)
+# print(gl.topological_sort())
+
+
 # g = GraphMatrix(12)
 # g.add_vertex(5)
 # g.add_vertex(7)
